9199e1dd68180096e406a3df1612060231969e24
craig <cslchua12@gmail.com>
2024-07-30 21:43:02 +1000

    Updated login, register, and main html
    Added anchors to login and register in register and login pages respectively

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ class Login(FlaskForm):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template(index.html)
+    return render_template('index.html')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
