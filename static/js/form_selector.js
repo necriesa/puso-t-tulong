0d273c0dd3cffc774ca6d38d679159822b584d52
craig <cslchua12@gmail.com>
2024-07-31 16:19:02 +1000

    form change works but wrong form

--- static/js/form_selector.js
+++ static/js/form_selector.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#register");
 
-  document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+  document.querySelector("#linkRegister").addEventListener("click", e => {
     e.preventDefault();
     loginForm.classList.add("form-hidden")
     createAccountForm.classList.remove("form-hidden")
